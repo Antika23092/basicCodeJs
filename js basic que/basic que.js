5e2ff9b06355b536897a50d4db926823d5e5eaba
Antika Maurya <122625116+Antika23092@users.noreply.github.com>
2023-11-27 12:30:03 +0530

    file
    this is basic que of js

---
+++ js basic que/basic que.js
@@ -0,0 +1,17 @@
+//Que- Write a C program to print your name, date of birth, and mobile number.
+//Que- program to print your name, date of birth, and mobile number.
+//Que-  program to print a block F using the hash (#), where the F has a height of six characters and width of five and four characters.
+// And also print a very large 'C'.
+//Que- program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
+// Que- C program to compute the perimeter and area of a circle with a given radius.
+//Que- program to display multiple variables.
+//Que-program to convert specified days into years, weeks and days.
+//Note: Ignore leap year.
+//Que- program that accepts two integers from the user and calculates the sum of the two integers.
+// Que- program that accepts two integers from the user and calculates the product of the two integers.
+//Que-  program to calculate the distance between two points.
+//Que- program that reads 5 numbers and sums all odd values between them. 
+//Que-  program that reads two integers and checks whether they are multiplied or not.
+//Que- program that accepts three integers and finds the maximum of three. 
+//Que-  program that prints all even numbers between 1 and 50.
+//Que- program to compute the perimeter and area of a circle with a given radius.
